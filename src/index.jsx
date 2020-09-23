c4d8256f73becf5380b636b85c3cfe2958f0d03f
vitorsilvazup <vitor.silva@zup.com.br>
2020-09-23 12:49:15 -0300

    fixing font

--- src/index.jsx
+++ src/index.jsx
@@ -1,22 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom'
 import App from 'containers';
-import { IntlProvider } from 'react-intl'
-
-import ptMessages from './i18n/pt'
-import esMessages from './i18n/es'
-
-import "@babel/polyfill"
-
-const getPreferredLanguage = () => {
-    const predefinedLanguages = ['pt', 'es']
-    const userLanguage = navigator.language || navigator.userLanguage
-    const language = userLanguage.split('-')[0]
-    return predefinedLanguages.includes(language) ? language : predefinedLanguages[0]
-}
-
-const locale = getPreferredLanguage()
-const messages = { ...ptMessages, ...esMessages }
 
 ReactDOM.render(
     <IntlProvider locale={locale} messages={messages[locale]}>
